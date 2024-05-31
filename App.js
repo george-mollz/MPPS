832fedb2cc64d58234c884e9ec181540a5128256
george-mollz <gmollel74@gmail.com>
2024-05-31 09:45:04 +0300

    removed the Welcome Page

--- App.js
+++ App.js
@@ -7,13 +7,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import SignUp2 from './src/screens/SignUp2';
 import Login from './src/screens/Login';
 import FinishedReg from './src/screens/FinishedReg';
-import Welcome from './src/screens/Welcome';
 import SignUp from './src/screens/SignUp';
-
 import Getstarted from './src/screens/Getstarted';
 import AppNav  from './src/screens/AppNav';
 import RequestForm from './src/screens/RequestForm';
-
 import BusinessLicenseVerification from './src/screens/BusinessLicenseVerification';
 import History from './src/screens/History';
 
@@ -27,11 +24,11 @@ export default function App() {
   const Stack = createStackNavigator();
 
   const StackNavigator = () => (
-    <Stack.Navigator initialRouteName='Welcome'  screenOptions={{
+    <Stack.Navigator initialRouteName='Getstarted'  screenOptions={{
       headerShown: false
       
      }}>
-        <Stack.Screen name='Welcome'   component={Welcome} />       
+           
         <Stack.Screen name='Getstarted'   component={Getstarted} />  
         <Stack.Screen name='Login'   component={Login} /> 
         <Stack.Screen name='SignUp'   component={SignUp} />  
